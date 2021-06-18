61dd6093f815c1bf98dd475985363c3f74c6df6f
jinook929 <jinook929@gmail.com>
2021-06-18 14:57:56 -0400

    try oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 # Rails.application.config.middleware.use OmniAuth::Builder do
 #   provider :google_oauth2, Rails.application.credentials.google[:google_id], Rails.application.credentials.google[:google_secret]
 # end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, domain: 'https://exilic-ecclesia.herokuapp.com/'
+end
