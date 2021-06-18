db161a008d6e9a85ca395738be3d1c2a54628342
jinook929 <jinook929@gmail.com>
2021-06-18 15:28:52 -0400

    try oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, Rails.application.credentials.google[:google_id], Rails.application.credentials.google[:google_secret]
+# end
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.credentials.google[:google_id], Rails.application.credentials.google[:google_secret]
+  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET']
 end
 # Rails.application.config.middleware.use OmniAuth::Builder do
 #   provider :google_oauth2, domain: 'https://exilic-ecclesia.herokuapp.com/'
